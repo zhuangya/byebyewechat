2eae3de2c978595ceb6cbafb83b71674b0673ef8
Ya Zhuang <zhuangya@bytedance.com>
2017-03-15 18:46:41 +0800

    add: auto redirect for "block_type" kind of page

--- auto-redirect.js
+++ auto-redirect.js
@@ -1,4 +1,8 @@
 (function () {
   'use strict';
+  if (window.location.href.indexOf('block_type') > -1) {
+    window.location.href = decodeURIComponent(window.location.search.split('&').filter(x => x.startsWith('url'))[0].split('=')[1]);
+    return;
+  }
   window.location.href = document.body.getAttribute('url').replace(/&amp;/g, '&');
 }());
