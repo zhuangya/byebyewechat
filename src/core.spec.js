d61edbaafb943e00baf2b81a041e6be97fd5a287
Ya Zhuang <zhuangya@users.noreply.github.com>
2019-09-24 12:20:04 +0800

    test: update test case

--- src/core.spec.js
+++ src/core.spec.js
@@ -12,10 +12,22 @@ describe('core', () => {
     });
   });
 
+  it('should also support `support.weixin.qq.com`', () => {
+
+    const fakeInfo = {
+      url: 'https://support.weixin.qq.com/cgi-bin/mmspamsupport-bin/newredirectconfirmcgi?main_type=2&evil_type=20&source=2&url=http%3A%2F%2Fwww.voidcn.com%2Farticle%2Fp-vvkyrmar-bno.html#wechat_redirect'
+    };
+
+    expect(rewriteUrl(fakeInfo)).toStrictEqual({
+      redirectUrl: 'http://www.voidcn.com/article/p-vvkyrmar-bno.html#wechat_redirect'
+    });
+  });
+
   it('should match filter', () => {
     expect(filter).toStrictEqual({
       urls: [
-        '*://weixin110.qq.com/*'
+        '*://weixin110.qq.com/*',
+        '*://support.weixin.qq.com/*'
       ],
     });
   });
